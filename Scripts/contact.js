e8cf46c4d9e97907b0e7da2d33c0cbfaaf636a89
galaxydev999 <brandoozler@gmail.com>
2022-03-15 07:01:35 -0700

    fini

---
+++ Scripts/contact.js
@@ -0,0 +1,118 @@
+function validateForm() {
+
+    var name = document.forms["contact"]["yourName"];
+    var namelength = name.value.length;
+
+    var email = document.forms['contact']['email'].value;
+
+    var subject = document.forms['contact']['subject'].value;
+
+    var del = document.forms['contact']['message2'];
+
+
+    var error2 = document.getElementById("error2");
+    var error3 = document.getElementById("error3");
+    var error11 = document.getElementById("error11");
+    var error12 = document.getElementById("error12");
+    var error13 = document.getElementById("error13");
+    var error14 = document.getElementById("error14");
+    var error15 = document.getElementById("error15");
+    var error16 = document.getElementById("error16");
+    var error17 = document.getElementById("error17");
+
+
+    if (name.value == "") {
+        error2.style.display = "block";
+
+        alert('Please Enter Your Name');
+        return false;
+    }
+
+    if (name.value !== "") {
+        error2.style.display = "none";
+    }
+
+    if (name.value.length > 100) {
+        alert("Name cannot exceed 100 characters");
+        error3.style.display = "block";
+        return false;
+    }
+
+    if (name.value.length < 100) {
+        error3.style.display = "none";
+    }
+
+
+
+
+    if (email == "") {
+        error11.style.display = "block";
+        alert('Please fill out your email');
+        return false;
+    }
+
+    if (email !== "") {
+        error11.style.display = "none";
+
+    }
+
+    if (email.length > 100) {
+        error12.style.display = "block";
+        alert('Your email should not exceed 100 characters');
+        return false;
+    }
+
+
+    if (subject.length == 0) {
+        error13.style.display = 'block';
+        alert('Please fill out the subject');
+        return false
+    }
+
+    if (subject.length < 50) {
+        error15.style.display = 'block';
+        alert('Subject should not be shorter than 50 characters');
+        return false
+    }
+
+    if (subject.length > 250) {
+        error14.style.display = 'block';
+        alert('Subject should not exceed 250 characters');
+        return false;
+    }
+
+
+    if (del.value == "") {
+        console.log("hel")
+        alert("Please enter a message")
+        error16.style.display = "block";
+        return false;
+    }
+
+    if (del.value.length > 500) {
+        alert("Message should not exceed 500 characters");
+        error17.style.display = "block";
+        return false;
+    }
+
+    if (del.value.length < 500) {
+        error17.style.display = "none";
+    }
+
+    if (del.value !== "") {
+        error16.style.display = "none";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Scripts/order.js
@@ -0,0 +1,146 @@
+function validateForm() {
+    var cakeselect = document.forms["contact"]["cakes"].value;
+
+
+    var name = document.forms["contact"]["yourName"];
+    var namelength = name.value.length;
+
+    var message = document.forms["contact"]["message"].value;
+    var messagelength = message.length;
+
+    var date = document.forms["contact"]["date"].value;
+    console.log(date);
+
+    var del = document.forms["contact"]["deliverTo"];
+    console.log(del.value);
+
+
+
+
+
+
+
+    var error1 = document.getElementById("error1");
+    var error2 = document.getElementById("error2");
+    var error3 = document.getElementById("error3");
+    var error4 = document.getElementById("error4");
+    var error5 = document.getElementById("error5");
+    var error6 = document.getElementById("error6");
+    var error7 = document.getElementById("error7");
+    var error10 = document.getElementById("error10");
+
+
+
+    console.log(namelength + " Name Length");
+    console.log(messagelength + " Message Length");
+    console.log(date);
+
+
+    if (cakeselect == "placeholder") {
+        error1.style.display = "block";
+
+        alert("Please Select Our Product");
+        return false;
+    }
+
+    if (cakeselect !== "placeholder") {
+        error1.style.display = "none";
+
+    }
+
+
+
+
+    if (name.value == "") {
+        error2.style.display = "block";
+
+        alert('Please Enter Your Name');
+        return false;
+    }
+
+    if (name.value !== "") {
+        error2.style.display = "none";
+    }
+
+    if (name.value.length > 100) {
+        alert("Name cannot exceed 100 characters");
+        error3.style.display = "block";
+        return false;
+    }
+
+    if (name.value.length < 100) {
+        error3.style.display = "none";
+    }
+
+    if (message == "") {
+        error10.style.display = "block";
+        alert('Please enter a message');
+        return false;
+    }
+
+    if (message !== "") {
+        error10.style.display = "none";
+    }
+
+    if (message.length > 30) {
+        error4.style.display = "block";
+        alert('Message should not exceed 30 characters');
+        return false;
+    }
+
+    if (message.length < 30) {
+        error4.style.display = "none";
+    }
+
+
+    if (date == "") {
+        alert("Please fill out the delivery date")
+        console.log('delivery date');
+        error5.style.display = "block";
+        return false
+    }
+
+    if (date !== "") {
+        error5.style.display = "none";
+    }
+
+    if (del.value == "") {
+        console.log("hel")
+        alert("Please enter a delivery address")
+        error6.style.display = "block";
+        return false;
+    }
+
+    if (del.value.length > 500) {
+        alert("Address should not exceed 500 characters");
+        error7.style.display = "block";
+        return false;
+    }
+
+    if (del.value.length < 500) {
+        error7.style.display = "none";
+    }
+
+    if (del.value !== "") {
+        error6.style.display = "none";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
